e16fec2b2b0dbfe3ae000d24d227e4148919bb64
Venkadesh <111363943+Thiruvenkatraja@users.noreply.github.com>
2023-05-23 00:13:49 +0530

    Update DataGridLogics.ts

--- src/Utils/DataGridLogics.ts
+++ src/Utils/DataGridLogics.ts
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { cartData, filterDatas } from "../Redux/CartSlice";
 
 export const DataGridLogics = () => {
